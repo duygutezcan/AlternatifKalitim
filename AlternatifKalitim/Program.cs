87d1163a245e968e87c88a5a71c719882fdbe62f
SEM <SEM@DESKTOP-4GKM6HK>
2022-11-08 09:24:53 +0300

    Add project files.

---
+++ AlternatifKalitim/Program.cs
@@ -0,0 +1,25 @@
+﻿using BasitKalitim;
+
+Ogrenci o = new Ogrenci();
+o.Ad = "Duygu";
+o.Soyad = "Tezcan Kantar";
+//o.Adres = new AlternatifKalitim.Adres();
+o.Adres.Ilce = "Ataşehir";
+o.Adres.Sehir = "İstanbul";
+o.Adres.Cadde = "Atatürk";
+o.Adres.Sokak = "Mevlana";
+
+
+Console.WriteLine(o.FullName());
+foreach (var item in o.AdresAl())
+{
+    Console.WriteLine(item);
+}
+
+Egitmen e = new Egitmen();
+e.Ad = "Şamil";
+e.Soyad = "Yılmaz";
+e.Adres.Ilce = "Ümraniye";
+e.Adres.Sehir = "İstanbul";
+e.Adres.Cadde = "Atatürk";
+
---
+++ AlternatifKalitim/Temel.cs
@@ -0,0 +1,36 @@
+﻿using AlternatifKalitim;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasitKalitim
+{
+    internal class Temel
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+
+        public Temel()
+        {
+            Adres=new Adres();
+        }
+       
+        public string FullName()
+        {
+            return Ad + Soyad;
+        }
+        public Adres Adres { get; set; }
+        public List<string> AdresAl()
+        {
+            List<string> adres = new List<string>();
+            adres.Add(FullName());
+            adres.Add(Adres.Cadde);
+            adres.Add(Adres.Sokak);
+            adres.Add($"{Adres.Ilce}-{Adres.Sehir}");
+            return adres;
+        }
+    }
+}
